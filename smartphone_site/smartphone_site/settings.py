2d104db7f263aa1df0df419b66264aef6302dc80
addie-tyc <65952631+addie-tyc@users.noreply.github.com>
2021-06-05 23:41:14 +0800

    Added ssl certification

--- smartphone_site/smartphone_site/settings.py
+++ smartphone_site/smartphone_site/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["salephones.site"]
+ALLOWED_HOSTS = ["salephones.site", "127.0.0.1"]
 
 
 # Application definition
