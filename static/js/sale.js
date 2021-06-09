bd47b0265aadf56dc91503a7af2ecc2efdfc37af
addie-tyc <65952631+addie-tyc@users.noreply.github.com>
2021-06-09 17:13:51 +0800

    Deleted staticfiles

--- static/js/sale.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#images").addEventListener("change", function() {
-    if (document.querySelector("#images").files.length > 4) {
-        alert("最多只能上傳 4 張圖片");
-        document.querySelector("#images").value = ""
-        }
-    }
-)
