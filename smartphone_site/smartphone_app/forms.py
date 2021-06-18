71edc7c16c757a3cef6c26771f37bce10dc5805c
addie-tyc <65952631+addie-tyc@users.noreply.github.com>
2021-06-19 01:39:56 +0800

    Added util.py imported in forms.py

--- smartphone_site/smartphone_app/forms.py
+++ smartphone_site/smartphone_app/forms.py
@@ -7,6 +7,8 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+from .util import get_phones
+
 class SignUpForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
@@ -49,111 +51,6 @@ class LoginForm(forms.Form):
     )
 
 
-def get_phones():
-    url = "https://en.wikipedia.org/wiki/List_of_Android_smartphones"
-    resp = requests.get(url, verify=False)
-    soup = BeautifulSoup(resp.text, "html.parser")
-
-    tables = soup.find_all("table", class_="wikitable")
-    rows = []
-    for i in tables:
-        rows.extend(i.select("tbody tr th"))
-
-    phones = [a.text.partition("(")[0].strip() for a in rows]
-    phones = [ p for p in phones if len(p.split()) > 1]
-
-    brands = set()
-    for p in phones:
-        brands.add(p.split(" ")[0])
-    brands = list(brands)
-    brands = sorted(brands)
-    for b in range(len(brands)):
-        if brands[b] == "Asus":
-            brands = brands[b:]
-            break
-
-    phone_list = []
-    for b in brands:
-        if b == "Samsung":
-            for p in phones:
-                if p.split(" ")[0] == b:
-                    if "/" in p:
-                        p_lst = p.split("/")
-                        for series in p_lst[1:]:
-                            sub_p = p_lst[0].replace("5G", "").strip()
-                            if series == "+":
-                                phone = sub_p + series
-                            else:
-                                phone = sub_p + " " + series
-                            if len(phone) > 0:
-                                phone_list.append(phone)
-                        phone_list.append(sub_p)
-                    else:
-                        phone = p.replace("5G", "").strip()
-                        if len(phone) > 0:
-                            phone_list.append(phone)
-        elif b == "POCO":
-            for p in phones:
-                if p.split(" ")[0] == b:
-                    phone = p.replace("5G", "").strip()
-                    if len(phone) > 0:
-                        phone_list.append(phone)
-
-        elif b == "Pixel":
-            for p in phones:
-                if p.split(" ")[0] == b:
-                    phone = p.replace("5G", "").strip()
-                    if len(phone) > 0:
-                        phone_list.append(phone)
-        else:
-            for p in phones:
-                if p.split(" ")[0] == b:
-                    if "/" in p:
-                        p_lst = p.split("/")
-                        for series in p_lst[1:]:
-                            sub_p = p_lst[0].replace("5G", "").strip()
-                            if series == "+":
-                                phone = sub_p + series
-                            else:
-                                phone = sub_p + " " + series
-                            if len(phone) > 0:
-                                phone_list.append(phone)
-                        phone_list.append(sub_p)
-                    else:
-                        phone = p.replace("5G", "").strip()
-                        if len(phone) > 0:
-                            phone_list.append(phone)
-                        phone_list.append(phone)
-    phone_list.append("Asus ROG Phone 2")
-
-    url = "https://www.theiphonewiki.com/wiki/List_of_iPhones"
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, "html.parser")
-
-    table = soup.find("div", id="toc")
-    iphones = []
-    for span in table.select("ul li span"):
-        if "iPhone" in str(span):
-            if ("SE" in str(span)) and ("1" in str(span)):
-                iphones.append("iPhone SE")
-            elif ("SE" in str(span)) and ("2" in str(span)):
-                iphones.append("iPhone SE2")
-            else:
-                iphones.append(span.text)
-    iphones.remove("iPhone")
-    phone_list.extend(iphones)
-    phone_set = set()
-    for p in phone_list:
-        phone_set.add(p)
-    phone_list.append("Samsung Galaxy A71")
-    phone_list = sorted(list(phone_set))
-    phone_list.remove("Asus ROG Phone II")
-    phone_list.remove("Release date")
-    phone_list.remove("Samsung Galaxy")
-    phone_list.remove("Samsung Galaxy A51 A71")
-    phone_list.remove("Samsung Galaxy A51 A71 5G")
-    phone_list.remove("Samsung Galaxy A52 A52 5G")
-    return phone_list
 
 class SaleForm(forms.ModelForm):
 
